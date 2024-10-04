fcd49682846be0f5555d3cdba1cf3c7544e1ad5e
cifumo <mtiosaputra70@gmail.com>
2024-10-05 05:27:38 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -467,6 +467,99 @@ ubah_background() {
   clear
   exit 0
 }
+hapus_background() {
+  echo -e "                                                       "
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "${BLUE}[+]                  HAPUS BACKGROUND                  [+]${NC}"
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+  
+  # Path ke file yang akan diubah
+  file_path="/var/www/pterodactyl/resources/views/templates/wrapper.blade.php"
+
+  # Konten baru untuk file
+  cat << 'EOF' > "$file_path"
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{{ config('app.name', 'Pterodactyl') }}</title>
+
+        @section('meta')
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+            <meta name="csrf-token" content="{{ csrf_token() }}">
+            <meta name="robots" content="noindex">
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+            <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+            <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+            <link rel="manifest" href="/favicons/manifest.json">
+            <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
+            <link rel="shortcut icon" href="/favicons/favicon.ico">
+            <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+            <meta name="theme-color" content="#0e4688">
+        @show
+
+        @section('user-data')
+            @if(!is_null(Auth::user()))
+                <script>
+                    window.PterodactylUser = {!! json_encode(Auth::user()->toVueObject()) !!};
+                </script>
+            @endif
+            @if(!empty($siteConfiguration))
+                <script>
+                    window.SiteConfiguration = {!! json_encode($siteConfiguration) !!};
+                </script>
+            @endif
+        @show
+        <style>
+            @import url('//fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap');
+            @import url('//fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:500&display=swap');
+        </style>
+
+        @yield('assets')
+
+        @include('layouts.scripts')
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('app.google_analytics', 'Pterodactyl') }}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '{{ config('app.google_analytics', 'Pterodactyl') }}');
+        </script>
+    </head>
+    <body class="{{ $css['body'] ?? 'bg-neutral-50' }}">
+        @section('content')
+            @yield('above-container')
+            @yield('container')
+            @yield('below-container')
+        @show
+        @section('scripts')
+            {!! $asset->js('main.js') !!}
+        @show
+    </body>
+</html>
+EOF
+
+  # Memeriksa apakah penggantian berhasil
+  if [ $? -eq 0 ]; then
+      echo -e "${GREEN}[+] =============================================== [+]${NC}"
+      echo -e "${GREEN}[+]               BACKGROUND BERHASIL DI HAPUS          [+]${NC}"
+      echo -e "${GREEN}[+] =============================================== [+]${NC}"
+  else
+      echo -e "${RED}[+] =============================================== [+]${NC}"
+      echo -e "${RED}[+]          TERJADI KESALAHAN SAAT MEMPERBARUI        [+]${NC}"
+      echo -e "${RED}[+]       SILAHKAN HUBUNGI 085263390832 UNTUK BANTUAN   [+]${NC}"
+      echo -e "${RED}[+] =============================================== [+]${NC}"
+  fi
+
+  sleep 2
+  clear
+  exit 0
+}
 hackback_panel() {
   echo -e "                                                       "
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
@@ -650,83 +743,7 @@ while true; do
       ubah_background
       ;;
       11)
-# Path ke file yang akan diubah
-file_path="/var/www/pterodactyl/resources/views/templates/wrapper.blade.php"
-
-# Konten baru untuk file
-cat << 'EOF' > "$file_path"
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>{{ config('app.name', 'Pterodactyl') }}</title>
-
-        @section('meta')
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-            <meta name="csrf-token" content="{{ csrf_token() }}">
-            <meta name="robots" content="noindex">
-            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-            <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
-            <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
-            <link rel="manifest" href="/favicons/manifest.json">
-            <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
-            <link rel="shortcut icon" href="/favicons/favicon.ico">
-            <meta name="msapplication-config" content="/favicons/browserconfig.xml">
-            <meta name="theme-color" content="#0e4688">
-        @show
-
-        @section('user-data')
-            @if(!is_null(Auth::user()))
-                <script>
-                    window.PterodactylUser = {!! json_encode(Auth::user()->toVueObject()) !!};
-                </script>
-            @endif
-            @if(!empty($siteConfiguration))
-                <script>
-                    window.SiteConfiguration = {!! json_encode($siteConfiguration) !!};
-                </script>
-            @endif
-        @show
-        <style>
-            @import url('//fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap');
-            @import url('//fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:500&display=swap');
-        </style>
-
-        @yield('assets')
-
-        @include('layouts.scripts')
-
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('app.google_analytics', 'Pterodactyl') }}"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '{{ config('app.google_analytics', 'Pterodactyl') }}');
-        </script>
-    </head>
-    <body class="{{ $css['body'] ?? 'bg-neutral-50' }}">
-        @section('content')
-            @yield('above-container')
-            @yield('container')
-            @yield('below-container')
-        @show
-        @section('scripts')
-            {!! $asset->js('main.js') !!}
-        @show
-    </body>
-</html>
-EOF
-
-# Memeriksa apakah penggantian berhasil
-if [ $? -eq 0 ]; then
-    echo "BACKROUND ANDA BERHASIL DI HAPUS"
-else
-    echo "TERJADI KESALAHAN SAAT MEMPERBARUI FILE!! SILAHKAN HUBUNGI 085263390832 UNTUK MEMINTA BANTUAN"
-fi
- ;;
+      hapus_background
     x)
       echo "Keluar dari skrip."
       exit 0
